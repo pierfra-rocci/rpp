539e701c3045872afd444bb987be1c1f28d599ca
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-03-24 22:44:19 +0100

    Update RapasApp.bat to use the correct path for Python executable

--- RapasApp.bat
+++ RapasApp.bat
@@ -1,4 +1,4 @@
 @echo off
 echo Starting Rapas App...
 cd C:\Users\pierf\photometria
-C:\Users\pierf\astronomy\.astronomy\python.exe streamlit_launcher.py
+C:\Users\pierf\astronomy\.astronomy\Script\python.exe streamlit_launcher.py
