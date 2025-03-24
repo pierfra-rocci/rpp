96d158e36c81ff76d6562f3f769e32fdef803b7a
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-03-24 21:54:21 +0100

    Enhance Streamlit hook to collect data files in addition to metadata

--- hooks/hook_streamlit.py
+++ hooks/hook_streamlit.py
@@ -1,3 +1,5 @@
 from PyInstaller.utils.hooks import copy_metadata
+from PyInstaller.utils.hooks import collect_data_files
 
 datas = copy_metadata('streamlit')
+datas += collect_data_files('streamlit')
