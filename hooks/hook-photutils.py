936006178bf33d5ab35c479fcea94957d6896f65
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-03-24 22:18:15 +0100

    Refactor PyInstaller hook for photutils: reintroduce data file collection and add support for binary files

--- hooks/hook-photutils.py
+++ hooks/hook-photutils.py
@@ -1,4 +1,6 @@
-from PyInstaller.utils.hooks import collect_data_files, collect_submodules, copy_metadata
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules, copy_metadata, get_package_paths
+import os
+import glob
 
 # Get all data files
 datas = collect_data_files('photutils')
@@ -14,3 +16,8 @@ hiddenimports += [
     'photutils.geometry.elliptical_overlap',
     'photutils.geometry.rectangular_overlap'
 ]
+
+# Add binary files (.pyd files for Windows)
+pkg_base, pkg_dir = get_package_paths('photutils')
+pyd_files = glob.glob(os.path.join(pkg_dir, '**', '*.pyd'), recursive=True)
+binaries = [(f, os.path.join('photutils', os.path.relpath(os.path.dirname(f), pkg_dir))) for f in pyd_files]
