4e1f19f36dd6a10f532879b25cf8053e0e764fe0
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-04-11 10:41:34 +0200

    Update astrometry refinement message to reflect GAIA DR3

--- pfr_app.py
+++ pfr_app.py
@@ -1358,7 +1358,7 @@ def find_sources_and_photometry_streamlit(image_data, _science_header, mean_fwhm
         st.warning("No sources found!")
         return None, None, daofind, bkg
     
-    st.info("Doing astrometry refinement with GAIA DR3...")
+    st.info("Doing astrometry refinement with GAIA DR3...") 
     # ra0, dec0, sr0 = astrometry.get_frame_center(wcs=w, width=image_data.shape[1], height=image_data.shape[0])
     # cat = catalogs.get_cat_vizier(ra0, dec0, sr0, 'gaiadr3', filters={'Rpmag':'<19'})
     # cat_col_mag = 'Rpmag'
