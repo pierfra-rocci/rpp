e6b12835e597c0919462dcabf11d8a218e2d54e6
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-04-19 22:37:22 +0200

    Add author, email, license, and description metadata to __version__.py

--- __version__.py
+++ __version__.py
@@ -2,3 +2,7 @@ version = "0.2.0"
 release = False
 version_info = (0, 2, 0)
 __all__ = ["version", "release", "version_info"]
+__author__ = "Pier-Francesco Rocci"
+__email__ = "pierfrancesco.rocci@gmail.com"
+__license__ = "MIT"
+__description__ = "A Python package for the analysis of the RAPAS images."
