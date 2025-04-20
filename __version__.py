b45d8252cbdd8f2bfccec4f2343bb3a57e5cf1d4
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-04-20 07:52:15 +0200

    Bump version to 0.2.2.dev0 and update version_info tuple for clarity

--- __version__.py
+++ __version__.py
@@ -1,6 +1,10 @@
-version = "0.2.0"
+version = "0.2.2.dev0"
 release = False
-version_info = (0, 2, 0)
+version_info = (0, 2, 2, 0, "dev0")
+# The version is a tuple of the form (major, minor, micro, release level, serial)
+# where release level is one of 'alpha', 'beta', 'candidate', or 'final'
+# and serial is an integer that is incremented for each release of that level.
+# For example, (0, 2, 2, 'alpha', 0) would be the first alpha release of version 0.2.2.
 __all__ = ["version", "release", "version_info"]
 __author__ = "Pier-Francesco Rocci"
 __email__ = "pierfrancesco.rocci@gmail.com"
