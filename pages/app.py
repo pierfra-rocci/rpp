Fix zero point calculation to handle masked values

pages/app.py
@@ -31,7 +31,6 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 import numpy as np
-from numpy.ma import MaskedConstant
 from astropy.io import fits
 from astropy.stats import sigma_clip, SigmaClip
 
@@ -1503,9 +1502,9 @@ def calculate_zero_point(_phot_table, _matched_table, gaia_band, air):
         zero_point_std = np.std(clipped_zero_points)
 
         # Fix: handle MaskedConstant
-        if isinstance(zero_point_value, MaskedConstant) or np.isnan(zero_point_value):
+        if isinstance(zero_point_value, np.ma.masked) or np.isnan(zero_point_value):
             zero_point_value = float('nan')
-        if isinstance(zero_point_std, MaskedConstant) or np.isnan(zero_point_std):
+        if isinstance(zero_point_std, np.ma.masked) or np.isnan(zero_point_std):
             zero_point_std = float('nan')
 
         _matched_table["calib_mag"] = (