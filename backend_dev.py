50148f01ff1e0f1c61ae0342508e1f223ab3b824
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-04-28 22:32:06 +0200

    Remove unnecessary host specification in app.run and serve calls

--- backend_dev.py
+++ backend_dev.py
@@ -182,8 +182,7 @@ def internal_server_error(e):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",
-            port=5000,
+    app.run(port=5000,
             debug=True,
             use_reloader=True,
             threaded=True)
