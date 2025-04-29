5bd900b5fc2c5129492ae42c94a85d23cc4da811
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-04-29 21:19:38 +0200

    Update color index filter in cross_match_with_gaia function to improve data accuracy

--- pages/app.py
+++ pages/app.py
@@ -1409,7 +1409,7 @@ def cross_match_with_gaia(
         mag_filter = (gaia_table[filter_band] < filter_max_mag)
 
         var_filter = gaia_table["phot_variable_flag"] != "VARIABLE"
-        color_index_filter = gaia_table["bp_rp"] < 2.0
+        color_index_filter = gaia_table["bp_rp"] < 1.5
         combined_filter = mag_filter & var_filter & color_index_filter
 
         gaia_table_filtered = gaia_table[combined_filter]
