be7b244629a02425b6a1220147f34c18d577dae2
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-05-02 15:28:55 +0200

    Remove unused PyInstaller hooks for photutils and streamlit

--- old_app/hooks/hook_streamlit.py
+++
@@ -1,5 +0,0 @@
-from PyInstaller.utils.hooks import copy_metadata
-from PyInstaller.utils.hooks import collect_data_files
-
-datas = copy_metadata("streamlit")
-datas += collect_data_files("streamlit")
