4c7cc94f86d00d3d24380362af9323c358ecb847
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-06-28 22:54:46 +0200

    Increase minimum adaptive box size for improved background estimation in detection and photometry function

--- src/pipeline.py
+++ src/pipeline.py
@@ -1674,7 +1674,7 @@ def detection_and_photometry(
     )
 
     height, width = image_data.shape
-    adaptive_box_size = max(32, min(height // 10, width // 10, 128))
+    adaptive_box_size = max(64, min(height // 10, width // 10, 128))
     adaptive_filter_size = min(11, adaptive_box_size // 2)
 
     bkg, bkg_error = estimate_background(
