f8516e73c2cd750bbb5399499f4fb8c26541f923
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-11-26 21:57:05 +0100

    ok

--- src/__version__.py
+++ src/__version__.py
@@ -1,7 +1,7 @@
-version = "1.4.5"
+version = "1.4.6"
 
 release = False
-version_info = (1, 4, 5)
+version_info = (1, 4, 6)
 # The version_info tuple is used to compare versions in a more granular way.
 __all__ = ["version", "release", "version_info"]
 __author__ = "Pier-Francesco Rocci"
