23022953ded6b1273ef84b8dec35efdaf4873ba4
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2025-12-29 13:29:40 +0100

    ok

--- src/transient.py
+++ src/transient.py
@@ -101,8 +101,6 @@ def find_candidates(
         fwhm=0.5*fwhm*pixel_scale,
         time=header.get('DATE-OBS', None),
         skybot=True,
-        vizier=["gaiaedr3", "ps1", "skymapper",
-                "sdss", "vsx", "apass"],
         ned=False,
         verbose=True,
         flagged=True,
