df7a7b2af90fffffca0143c7915456cf2dc607cb
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2026-01-05 22:15:59 +0100

    Bump version to 1.5.2 and update version_info tuple

--- src/__version__.py
+++ src/__version__.py
@@ -1,7 +1,7 @@
-version = "1.5.1"
+version = "1.5.2"
 
 release = False
-version_info = (1, 5, 1)
+version_info = (1, 5, 2)
 # The version_info tuple is used to compare versions in a more granular way.
 __all__ = ["version", "release", "version_info"]
 __author__ = "Pier-Francesco Rocci"
