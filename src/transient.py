4182f1b701ebd7f24679db1390df5264243e799b
pierfra-rocci <pierfrancesco.rocci@gmail.com>
2026-01-06 22:22:08 +0100

    Fix formatting in plot_cutout function to ensure proper rendering of magnitude calibration in titles

--- src/transient.py
+++ src/transient.py
@@ -395,7 +395,7 @@ def plot_cutout(
             if 'mag_limit' in cutout['meta']:
                 title += ' : limit %.2f' % cutout['meta']['mag_limit']
             if 'mag_calib' in cutout['meta']:
-                title += ' : mag = %.2f $\pm$ %.2f' % (
+                title += r' : mag = %.2f $\pm$ %.2f' % (
                     cutout['meta'].get('mag_calib', np.nan),
                     cutout['meta'].get(
                         'mag_calib_err', cutout['meta'].get('magerr', np.nan)
